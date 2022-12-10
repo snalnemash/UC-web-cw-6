f29ed243450c887be7211617237365d2c93f5ec9
salnemash <sn.alnemash2@gmail.com>
2022-12-10 22:55:53 +0300

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+function BMI(weight, height) {
+  let result = weight / (height * height);
+  return result;
+}
+console.log(BMI(160, 50));
+
+BMI(BMI(160, 50));
+
+function status(bmi) {
+  if (bmi < 18.5) {
+    return "لديك نقص في الوزن";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "وزنك صحي";
+  } else if (bmi >= 25) {
+    return "لديك زيادة في الوزن";
+  }
+}
+
+function calculate() {
+    document.getElementById(...).value
+}
+let result = weight / (height * height);
+let result = weight / (height * height);
